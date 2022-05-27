d4a82ff892469e66af57f4fb0700ad2faccb1abb
Kevin M Granger <git@kevinmgranger.me>
2022-05-27 09:52:00 -0400

    Move deploy time metric to common deploytime module

---
+++ exporters/deploytime/__init__.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+
+
+@dataclass(kw_only=True, frozen=True)
+class DeployTimeMetric:
+    """
+    While it is called image_sha for backwards compatibility,
+    it could also refer to the release hash for a github release.
+    """
+
+    name: str
+    namespace: str
+    # WARNING: do not mutate the dict after hashing or things may break.
+    labels: dict[str, str]
+    deploy_time: object
+    image_sha: str
+
+    def __hash__(self):
+        return hash(
+            (
+                self.name,
+                self.namespace,
+                hash(tuple(self.labels.items())),
+                self.deploy_time,
+                self.image_sha,
+            )
+        )
