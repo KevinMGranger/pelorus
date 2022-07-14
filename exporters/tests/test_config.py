57efd2f3237453ddbbabb17eaf163e30aa11d3e1
Kevin M Granger <kgranger@redhat.com>
2022-07-13 20:17:33 -0400

    Add example

---
+++ exporters/tests/test_config.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass, field
+from typing import Optional, cast
+
+from pelorus.config import NoEnv, var
+
+# TODO: typing on defaults is wonky.
+
+
+@dataclass
+class ExampleCommittimeConfig:
+    kube_client: object = var(env_lookups=NoEnv)
+    api_user: Optional[str] = var(
+        default=None, env_lookups=["API_USER", "GIT_USER", "GITHUB_USER", "USER"]
+    )
+    # token: Optional[str] = var(
+    #     default=None, env_lookups=["TOKEN", "GIT_TOKEN", "GITHUB_TOKEN"]
+    # )
+    token: Optional[str] = var(
+        default=cast(Optional[str], None),
+        env_lookups=["TOKEN", "GIT_TOKEN", "GITHUB_TOKEN"],
+    )
+
+    git_api: str = var(default="", env_lookups="GIT_API GITHUB_API".split())
+    git_provider: str = "github"
+
+    tls_verify: bool = True
+
+    namespaces: set[str] = field(default_factory=set)
