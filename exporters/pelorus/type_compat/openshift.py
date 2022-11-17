420403b370d30fe7b1c02f58a4e74d6e84b3259a
Kevin M Granger <kgranger@redhat.com>
2022-11-17 15:07:35 -0500

    Beginto experiment with cattrs
    Signed-off-by: Kevin M Granger <kgranger@redhat.com>

--- exporters/pelorus/type_compat/openshift.py
+++ exporters/pelorus/type_compat/openshift.py
@@ -6,37 +6,25 @@ that untyped, dynamic data from openshift "fits" these shapes.
 These should probably be type stubs only, but I can't figure
 out how to do that yet.
 """
-from typing import Any, Generic, TypeVar
+from typing import Generic, TypeVar
 
-from openshift.dynamic.client import DynamicClient
-
-
-class ResourceInstance:
-    """
-    A resource as returned by the openshift library.
-
-    `client` highlights that it still references the client,
-    and the `__getattribute__ -> Any` makes the type checker
-    not complain about attribute accesses.
-    """
-
-    client: DynamicClient
-
-    def __getattribute__(self, __name: str) -> Any:
-        ...
+import attrs
 
 
+@attrs.define
 class Metadata:
     """
     OpenShift metadata. Almost always guaranteed to be present.
     """
 
     name: str
+    namespace: str
     labels: dict[str, str]
     annotations: dict[str, str]
 
 
-class CommonResourceInstance(ResourceInstance):
+@attrs.define
+class CommonResourceInstance:
     "Resource instances that we work with usually have the typical metadata."
     apiVersion: str
     kind: str
@@ -46,14 +34,25 @@ class CommonResourceInstance(ResourceInstance):
 R = TypeVar("R", bound=CommonResourceInstance)
 
 
+@attrs.define
 class CommonResourceInstanceList(CommonResourceInstance, Generic[R]):
     "We work with lists a lot. This lets us easily mark what they contain."
     items: list[R]
 
 
-__all__ = [
-    "ResourceInstance",
-    "Metadata",
-    "CommonResourceInstance",
-    "CommonResourceInstanceList",
-]
+@attrs.define
+class Build(CommonResourceInstance):
+    @attrs.define
+    class Output:
+        @attrs.define
+        class To:
+            pass
+
+    # spec: BuildSpec
+
+
+# __all__ = [
+#     "Metadata",
+#     "CommonResourceInstance",
+#     "CommonResourceInstanceList",
+# ]
